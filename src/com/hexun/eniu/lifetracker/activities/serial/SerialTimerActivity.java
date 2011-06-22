c52e65f1aed4a099546c429c0175a27e4d2520e9
eniu9350 <niujin@gmail.com>
2011-06-22 13:41:18 +0800

    nothing

--- src/com/hexun/eniu/lifetracker/activities/serial/SerialTimerActivity.java
+++ src/com/hexun/eniu/lifetracker/activities/serial/SerialTimerActivity.java
@@ -1,38 +1,46 @@
 package com.hexun.eniu.lifetracker.activities.serial;
 
-import com.hexun.eniu.lifetracker.R;
+import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
+import com.hexun.eniu.lifetracker.R;
+import com.hexun.eniu.lifetracker.persistence.DbManager;
+
 public class SerialTimerActivity extends Activity {
+	private DbManager dbm;
 	private Chronometer crono;
 
+	private String targetName ;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+//		if(dbm == null)	{ 
+			dbm = new DbManager(this);
+//		}
 
 		setContentView(R.layout.serial_timer);
 
-		// new
-		// AlertDialog.Builder(this).setMessage("mymessage").setTitle("title")
-		// .setCancelable(true).setNeutralButton(android.R.string.cancel,
-		// new DialogInterface.OnClickListener() {
-		// public void onClick(DialogInterface dialog,
-		// int whichButton) {
-		// }
-		// }).show();
+//		new AlertDialog.Builder(this).setMessage("mymessage").setTitle("title")
+//				.setCancelable(true).setNeutralButton(android.R.string.cancel,
+//						new DialogInterface.OnClickListener() {
+//							public void onClick(DialogInterface dialog,
+//									int whichButton) {
+//							}
+//						}).show();
 
 		Intent intent = getIntent();
-		String targetName = intent.getStringExtra("targetName");
+		targetName = intent.getStringExtra("targetName");
 
 		TextView tvTargetName = (TextView) this
 				.findViewById(R.id.SerialTimer_tvTargetName);
@@ -42,9 +50,16 @@ public class SerialTimerActivity extends Activity {
 		crono.setBase(SystemClock.elapsedRealtime());
 		startCrono();
 
-		Button next = (Button) findViewById(R.id.SerialTimer_btStop);
-		next.setOnClickListener(new View.OnClickListener() {
+		Button stop = (Button) findViewById(R.id.SerialTimer_btStop);
+		stop.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
+				
+				ArrayList<ArrayList<Object>> l = dbm.getAllRowsAsArrays();
+				Log.i("====before insert===", "count="+l.size());
+				
+				dbm.addRow(targetName, "nothing");
+				
+				
 				crono.stop();
 
 				long elapsedMillis = SystemClock.elapsedRealtime()
